8da8f0fadd3ab7d6a0bd2d2c8d4c38caab5fb6a1
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2025-09-07 00:28:19 +0900

    Created Maven project for member-service

---
+++ src/test/java/com/cu2mber/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu2mber.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
